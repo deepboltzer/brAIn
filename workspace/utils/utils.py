7b3d2f78576bb18471a0bacc644f84ab9322ddc1
Tobias-Huerten <s6tohuer@uni-bonn.de>
2022-03-26 20:54:52 +0100

    Add plot_log function to utils.py

--- workspace/utils/utils.py
+++ workspace/utils/utils.py
@@ -1,7 +1,9 @@
 from email import header
 from math import factorial
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import gym
 from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
@@ -47,3 +49,15 @@ def load_log(file_path):
     df = pd.DataFrame(data).set_index('timesteps')
 
     return df
+
+
+def plot_log(data, title, xlabel='timesteps', ylabel='mean reward', figsize=(9,5), axis=[0, 500000, -100, 750]):
+  plt.figure(figsize=figsize)
+  plt.plot(data.index, data['mean_rew'], color='darkslategray', alpha=1, zorder=0)
+  plt.vlines(data.index, ymin=0, ymax=data['mean_rew'], color=np.where(data['mean_rew'] < 0, 'maroon', 'darkgreen'), alpha=1, zorder=1)
+  plt.scatter(data.index, data['mean_rew'], color=np.where(data['mean_rew'] < 0, 'maroon', 'darkgreen'), marker='D', alpha=1, zorder=2)
+  plt.title(title)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.axis(axis) # plt.axis([xmin, xmax, ymin, ymax])
+  plt.show()
