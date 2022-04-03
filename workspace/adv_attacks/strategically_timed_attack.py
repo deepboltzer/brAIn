d282c6cff918dcc89470fd16b26f10fd2030092a
Tobias-Huerten <s6tohuer@uni-bonn.de>
2022-04-03 15:53:31 +0200

    Implement c function

--- workspace/adv_attacks/strategically_timed_attack.py
+++ workspace/adv_attacks/strategically_timed_attack.py
@@ -1,4 +1,5 @@
 import torch
+import copy
 
 from base_attack import Base_Attack
 
@@ -19,12 +20,24 @@ class StrategicallyTimedAttack(Base_Attack):
             
             orig_obs = self.data.last_obs
             orig_act, _states = self.predict(orig_obs)
+            adv_sample = self.craft_sample(orig_obs, orig_act)
+            perturbed_act, _states = self.predict(adv_sample)
             if self.c() >= self.beta:
-                adv_sample = self.craft_sample(orig_obs, orig_act)
-                perturbed_act, _states = self.predict(adv_sample)
                 self.perform_step(perturbed_act)
             else:
                 self.perform_step(orig_act)
     
-    def c(self):
-        pass
+    def c(self, orig_act, perturbed_act):
+        """
+        Action preference function. 
+        The higher the value the more this action is preferred over the other.
+        Therefore the higher the value the more efficient is an attack in this timestep.
+        :param orig_act: original action on unperturbed observation
+        :param perturbed_act: action chosen on adversarial sample
+        """
+        env_copy1 = copy.deepcopy(self.env)
+        env_copy2 = copy.deepcopy(self.env)
+        _state, max_act, _done, _info = env_copy1.step(orig_act)
+        _state, min_act, _done, _info = env_copy1.step(orig_act)
+
+        return max_act - min_act
